509a8d06a931092ce90d65d192af451180cc16b6
SioChan1 <palivonaite95@gmail.com>
2023-03-04 15:10:24 +0100

    update route creation

--- routes/champion.js
+++ routes/champion.js
@@ -27,3 +27,23 @@ router.get("/:id", (req, res) => {
     .catch(err => { res.status(500).send( {message: err.message }); })
 
 });
+
+router.put("/:id", (req, res) => {
+
+    const id = req.params.id;
+
+    champion.findByIdAndUpdate(id, req.body)
+    .then(data => { 
+        if (!data)
+        { 
+        res.status(404). send({ message: "Can not update the Champion with id=" + id + "Maybe the champion is lost in the void!"})
+        }
+        else
+        {
+            res.send({message: "Champion has been updated!"})
+        }
+     })
+    .catch(err => { res.status(500).send( {message: "Error updating champion with id=" +
+    id }); })
+
+});
