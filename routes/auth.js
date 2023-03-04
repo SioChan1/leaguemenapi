aa3acb06ea8e84c35ed8a422df9d88e9f28be4f6
SioChan1 <palivonaite95@gmail.com>
2023-03-04 19:06:38 +0100

    User validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,20 @@
 const router = require('express').Router();
 const user = require('../models/user');
+const { registerValidation } = require('../validation');
 
 router.post("/register", async (req, res) => {
 
-})
+    const { error } = registerValidation(req.body);
+
+    if (error) {
+        return res.status(400).json({error: error.details[0].message});
+    }
+
+    return res.status(200).json({msg: "Reguster route..."});
+});
 
 router.post("/login", async (req, res) => {
+    return res.status(200).json({msg: "Login route..."});
 
 })
 
