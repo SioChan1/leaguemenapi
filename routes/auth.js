77f663772749beabc703a990b601983c3c027463
SioChan1 <palivonaite95@gmail.com>
2023-03-05 15:22:28 +0100

    user creation, validation and error catching

--- routes/auth.js
+++ routes/auth.js
@@ -17,6 +17,22 @@ router.post("/register", async (req, res) => {
         return res.status(400).json({ error: "Email already taken"});
     }
 
+    const salt = await bcrypt.genSalt(10);
+    const password = await bcrypt.hash(req.body.password, salt);
+
+    const userObject = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password
+    });
+
+    try {
+        const savedUser = await userObject.save();
+        res.json({ error: null, data: savedUser._id });
+    } catch (error) {
+        res.status(400).json({ error })
+    }
+
 });
 
 router.post("/login", async (req, res) => {
