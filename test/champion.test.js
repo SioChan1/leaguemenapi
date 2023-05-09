73e614acb436fc0479e484f4b1309515fad084e6
SioChan1 <palivonaite95@gmail.com>
2023-05-10 00:01:54 +0200

    Boiler plate for mocha test

---
+++ test/champion.test.js
@@ -0,0 +1,12 @@
+process.env.NODE_ENV = 'test';
+
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('/First Test Collection', function(){
+
+    it('should test two values', function() {
+
+        //test content should go here 
+    })
+})
