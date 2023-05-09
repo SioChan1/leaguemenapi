dd783c464c16bac497512563711cd208427d0ca7
SioChan1 <palivonaite95@gmail.com>
2023-05-10 00:09:19 +0200

    Test of Mocha tests

--- test/champion.test.js
+++ test/champion.test.js
@@ -6,7 +6,10 @@ const expect = chai.expect;
 describe('/First Test Collection', function(){
 
     it('should test two values', function() {
-
         //test content should go here 
+        let expectedVal = 10;
+        let actualVal = 10;
+
+        expect(actualVal).to.be.equal(expectedVal);
     })
 })
