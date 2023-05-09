f0973f8ceac31d8dfeea50b6a011f5509f6a641e
SioChan1 <palivonaite95@gmail.com>
2023-05-10 00:43:49 +0200

    Mocha test error

--- test/champion.test.js
+++ test/champion.test.js
@@ -2,8 +2,13 @@ process.env.NODE_ENV = 'test';
 
 const chai = require('chai');
 const expect = chai.expect;
+const should = chai.should();
+const chaiHttp = require('chai-http');
+const server = require('../server');
 
-describe('/First Test Collection', function(){
+chai.use(chaiHttp);
+
+/*describe('/First Test Collection', function(){
 
     it('should test two values', function() {
         //test content should go here 
@@ -12,4 +17,25 @@ describe('/First Test Collection', function(){
 
         expect(actualVal).to.be.equal(expectedVal);
     })
-})
+})*/
+
+describe('/First Test Collection', () => {
+
+    it('test default API welcome route...', (done) => {
+
+        chai.request(server)
+        .get('/api/welcome')
+        .end((err, res) => {
+            res.should.have.status(200);
+        done();
+         });
+    });
+
+    it('should test two values.....', () => {
+        //test content should go here 
+        let expectedVal = 10;
+        let actualVal = 10;
+
+        expect(actualVal).to.be.equal(expectedVal);
+    })
+});
